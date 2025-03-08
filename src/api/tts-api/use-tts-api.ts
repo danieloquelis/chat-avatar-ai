f17bfa1887d0d6a5524ac53fed676f02350f8ab3
Daniel Oquelis <daniel.oquelis@gmail.com>
2025-03-08 22:06:23 +0100

    chore: renaming api route

--- src/api/tts-api/use-tts-api.ts
+++ src/api/tts-api/use-tts-api.ts
@@ -2,5 +2,8 @@ import { useMutation } from "@/hooks/use-mutation";
 import { TTSApiRequest, TTSApiResponse } from "./tts-api-common";
 
 export const useTTSApi = () => {
-  return useMutation<TTSApiResponse, TTSApiRequest>("/api/tts", "POST");
+  return useMutation<TTSApiResponse, TTSApiRequest>(
+    "/api/text-to-speech",
+    "POST",
+  );
 };
