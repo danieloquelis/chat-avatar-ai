017338cd801553ceb141ef08acc5589a8c8170c4
Daniel Oquelis <daniel.oquelis@gmail.com>
2025-03-15 19:28:59 +0100

    chore: add use-graph hook

--- src/components/avatar/use-avatar-model.ts
+++ src/components/avatar/use-avatar-model.ts
@@ -2,12 +2,12 @@ import { useGLTF } from "@react-three/drei";
 import { AvatarGLTF } from "./avatar-common";
 import React from "react";
 import { SkeletonUtils } from "three-stdlib";
-import { useGraph } from "@react-three/fiber";
+import { useGraph } from "@/hooks/use-graph";
 
 export const useAvatarModel = () => {
   const { scene, ...rest } = useGLTF("/assets/avatar.glb");
   const clone = React.useMemo(() => SkeletonUtils.clone(scene), [scene]);
-  const { nodes, materials } = useGraph(clone) as AvatarGLTF;
+  const { nodes, materials } = useGraph<AvatarGLTF>(clone);
 
   return {
     ...rest,
